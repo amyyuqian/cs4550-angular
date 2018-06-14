41942a12420debca4e5eff9ccc08ba2f5f1ffa22
amyyuqian <amyyuqian@gmail.com>
2018-06-14 10:11:42 -0400

    change node server constant to heroku url

--- src/app/commons/constants.ts
+++ src/app/commons/constants.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class Constants {
  BASE_API_URL = 'https://murmuring-dawn-26453.herokuapp.com/api';
- NODE_API_URL = 'http://localhost:4000/api';
+ NODE_API_URL = 'https://cs4550-aqian-node.herokuapp.com//api';
  USER_API_URL = this.NODE_API_URL + '/user';
  COURSE_API_URL = this.BASE_API_URL + '/course';
  LESSON_API_URL = this.BASE_API_URL + '/lesson';
